72de84a8d970f2732244d7051914070e0b361a2a
Avishek <avishek.palit96@gmail.com>
2022-12-22 11:52:07 +0600

    exam 2 complete

--- Exam2/Task1/OrderHistory.cs
+++ Exam2/Task1/OrderHistory.cs
@@ -18,31 +18,44 @@ namespace Task1
                            ("monir", 45)
                };*/
 
-        object[] persons = new { name = name, age = age,{
-                           ("jalaluddin", 41),
-            ("tareq", 33),
-            ("hasan", 52),
-            ("rashed", 21),
-            ("monir", 45)
-        } }
-    [];
+        object[] persons = new[]
+            {
+                new{name = "jalaluddin", age = 41 },
+                new { name = "tareq", age = 33 },
+                new { name = "hasan", age = 52 },
+                new { name = "rashed", age = 21 },
+                new { name = "monir", age = 45 }
+        
+    };
 
-    object[] orders = new { name = name, amount = amount, }[]
+
+    object[] orders = new[]
+    {
+                 
+                new { name = "monir", amount = 300.5 },
+                new { name = "rashed", amount = 20.5 },
+                new { name = "tareq", amount = 29.9 },
+                new { name = "hasan", amount = 17.7 },
+                new{name = "jalaluddin", amount = 30.2 }
+        
+    };
+
+/*object[] orders = new { name = name, amount = amount, }[]
         {
             ("monir", 300.5),
             ("rashed", 20.5),
             ("tareq", 29.9),
             ("hasan", 17.7),
             ("jalaluddin", 30.2)
-        };
+        };*/
 
         public List<(string, double)> GetOrders()
         {
             var result = new List<(string, double)>();
 
-            foreach (var person in persons)
+            foreach (dynamic person in persons)
             {
-                foreach(var order in orders)
+                foreach(dynamic order in orders)
                 {
                     if(person.name == order.name)
                     {
